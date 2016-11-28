b60d3709dffd96b083af01b1624722c9df72efdd
Exerosis <theexerosisgaming@gmail.com>
2016-11-27 21:00:14 -0500

    ~Finished Full Reviews View

--- app/src/main/java/me/exerosis/nanodegree/movies/implementation/controller/review/FullReviewActivity.java
+++ app/src/main/java/me/exerosis/nanodegree/movies/implementation/controller/review/FullReviewActivity.java
@@ -3,6 +3,7 @@ package me.exerosis.nanodegree.movies.implementation.controller.review;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import me.exerosis.nanodegree.movies.implementation.model.data.Review;
 import me.exerosis.nanodegree.movies.implementation.view.review.FullReviewView;
@@ -16,5 +17,12 @@ public class FullReviewActivity extends AppCompatActivity implements FullReviewC
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         view = new FullReviewView(this, (Review) getIntent().getParcelableExtra(ARG_REVIEW));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
 }
